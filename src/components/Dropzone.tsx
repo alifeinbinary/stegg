e2dc5406eb298d54bdd4d7039a4f97f600c684eb
Andrew Halliwell <studio@strangeways.ca>
2024-09-01 05:03:47 -0700

    chore: adding license headers to all Typescript files

--- src/components/Dropzone.tsx
+++ src/components/Dropzone.tsx
@@ -1,4 +1,20 @@
-// import { createRef } from "react"
+/*
+ *   Copyright (c) 2024 Andrew Halliwell
+
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faUpload } from "@fortawesome/free-solid-svg-icons"
 import { useDropzone } from "react-dropzone"
