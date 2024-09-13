ceb61c9533dce9f415abcd83d6a9e734ba552eb1
Andrew Halliwell <studio@strangeways.ca>
2024-09-12 22:10:55 -0700

    feat: preparing the Apollo client to communicate with two API sources and

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,56 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createHttpLink } from '@apollo/client';
 import App from './App.tsx'
-import { ApolloProvider, ApolloClient, InMemoryCache, HttpLink } from '@apollo/client'
+import { ApolloProvider, ApolloClient, InMemoryCache, ApolloLink } from '@apollo/client'
 import './index.css'
 
-const apiURL = import.meta.env.VITE_MANAGE_API_URL;
-const token = import.meta.env.VITE_TOKEN;
+const readApiHttpLink = createHttpLink({
+  uri: import.meta.env.VITE_READ_API_URL,
+});
+
+const mainApiHttpLink = createHttpLink({
+  uri: import.meta.env.VITE_MAIN_API_URL,
+});
+
+const authMiddleware = new ApolloLink((operation, forward) => {
+  const token = import.meta.env.VITE_TOKEN;
+  // add the authorization to the headers
+  operation.setContext(({ headers = {} }) => ({
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+      "x-tenant": "root"
+    }
+  }));
+
+  return forward(operation);
+});
 
 const client = new ApolloClient({
-  link: new HttpLink({
-    uri: apiURL,
-    headers: {
-      authorization: `Bearer ${token}`,
-    },
-  }),
-  cache: new InMemoryCache(),
+  link: authMiddleware.split(
+    operation => operation.getContext()["apiName"] === "main",
+    mainApiHttpLink,
+    readApiHttpLink
+  ),
+  connectToDevTools: true,
+  cache: new InMemoryCache({
+    typePolicies: {
+      Query: {
+        fields: {
+          fileManager: {
+            merge(existing = {}, incoming) {
+              // Merge logic here
+              return {
+                ...existing,
+                ...incoming
+              };
+            }
+          }
+        }
+      }
+    }
+  })
 });
 
 createRoot(document.getElementById('root')!).render(
