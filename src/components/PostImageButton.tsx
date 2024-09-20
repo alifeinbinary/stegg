1b3f913a631c83ab7fb3b5e9e450c48b81b8ebbe
Andrew Halliwell <studio@strangeways.ca>
2024-09-20 04:50:06 -0700

    feat: Post to Feed button now uploads image to S3, creates file reference in fileManager, and creates database entry for post with timestamp and author.

--- src/components/PostImageButton.tsx
+++ src/components/PostImageButton.tsx
@@ -15,37 +15,151 @@
  *   along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-// import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
-// import { useCreateBinaryImagePost } from "../utils/api";
-// import { PostImageButtonProps } from "../types";
-// import { clearContx } from "../utils/translate";
-// import { createPngWithMetadata } from "../utils/save";
-import { usePostState } from "../utils/stores";
+import { Tooltip } from "flowbite-react";
+import { useCreateBinaryFeedImage } from "../api/createBinaryFeedImage";
+import { useCreateBinaryImagePost } from "../api/createBinaryImagePost";
+import { useGetPreSignedPostPayload } from "../api/getPreSignedPostPayload";
+import { clearContx } from "../utils/translate";
+import { createPngWithMetadata } from "../utils/save";
+import { usePostState, useImageState } from "../utils/stores";
+import { toast } from "react-toastify";
+import { uploadFileToS3 } from "../api/uploadFileToS3";
 
 const PostImageButton: React.FC = () => {
 
     const {
-        user, setUser
+        author, setAuthor
     } = usePostState();
 
-    const handlePost = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const {
+        canvasRef,
+        input,
+        password,
+        encryptedText,
+        encryptionEnabled,
+        setOutput
+    } = useImageState();
+
+    const { getPreSignedPostPayload } = useGetPreSignedPostPayload();
+    const { createBinaryFeedImage } = useCreateBinaryFeedImage();
+    const { createBinaryImagePost } = useCreateBinaryImagePost();
+
+    const handlePostVisibility = () => {
+        if (encryptionEnabled) {
+            if (!input || !password) {
+                return true;
+            } else {
+                return false;
+            }
+        } else if (!encryptionEnabled) {
+            if (!input) {
+                return true;
+            } else {
+                return true;
+            }
+        } else {
+            return false;
+        }
+    };
+
+    const fetchBlobAndGetSize = async (url: string) => {
+        const response = await fetch(url);
+        const blob = await response.blob();
+        return { blob, size: blob.size };
+    };
+
+    const handleButtonClick = async (event: React.MouseEvent<HTMLButtonElement>) => {
         event.preventDefault();
-        console.log(user,);
-    }
+        const toastId = toast("Processing image...", { autoClose: false });
+
+        if (canvasRef && canvasRef.current && input) {
+
+            if (encryptionEnabled) {
+
+                toast.update(toastId, {
+                    render: "Embedding metadata...",
+                    type: "info",
+                    isLoading: true,
+                    autoClose: 2000
+                });
+
+                const { fileName, url } = await createPngWithMetadata(canvasRef.current, encryptedText, encryptionEnabled, password, "post");
+
+                console.debug("fileName", fileName, "url", url);
+
+                const { blob, size } = await fetchBlobAndGetSize(url);
+
+                console.debug("blob", blob, "size", size);
+
+                const { data } = await getPreSignedPostPayload({
+                    name: fileName,
+                    type: "image/png",
+                    size: size,
+                })
+
+                console.debug("data", data);
+
+                const preSignedPostPayload = data?.fileManager.getPreSignedPostPayload.data;
+
+                console.debug("preSignedPostPayload", preSignedPostPayload);
+
+                await uploadFileToS3(
+                    preSignedPostPayload,
+                    blob,
+                );
+
+                const fileInput = {
+                    ...preSignedPostPayload.file,
+                    tags: ["binary-image"],
+                    aliases: [],
+                    location: { folderId: "66dab76c9c00420008532f91" },
+                };
+                delete fileInput.__typename;
+
+                console.debug("fileInput", fileInput);
+
+                const createdFile = await createBinaryFeedImage(fileInput);
+
+                console.debug("createdFile", createdFile);
+
+                const authorOrAnon = author ? author : "Anon";
+                await createBinaryImagePost(authorOrAnon, preSignedPostPayload.file.key);
+
+            } else {
+                toast.update(toastId, {
+                    type: "warning",
+                    render: "Encryption disabled. Only encrypted messages can be posted.",
+                    isLoading: false,
+                    autoClose: 2000
+                });
+            }
+            clearContx(canvasRef);
+            setOutput([]);
+        } else {
+            console.debug("No canvasRef or input");
+            toast.update(toastId, {
+                type: "warning",
+                render: "No image to upload.",
+                isLoading: false,
+                autoClose: 2000
+            })
+        }
+    };
 
     return (
-        <div tabIndex={0} className='shadow-card p-1 flex h-[46px] items-center justify-center rounded-md text-gray-900 bg-gray-200'>
+        <div tabIndex={0} className='shadow-card p-1 flex h-[46px] items-center justify-center rounded-md text-gray-900 bg-gray-200 dark:bg-seablue'>
             <input onChange={(e) => {
-                setUser(e.target.value);
-            }} data-testid="user-input" value={user} type="text" tabIndex={0} id="user-input" disabled={false} className={`text-base rounded-none rounded-l-lg max-w-40 bg-gray-50 border text-gray-900 focus:ring-sagegreen focus:border-sagegreen block flex-1 min-w-0 w-full border-gray-300 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-900 dark:focus:border-blue-900`} placeholder={"User"} />
-
-            <button onClick={(e) => {
-                handlePost(e)
-            }} className="flex h-full w-20 ml-1 items-center justify-center transition ease-in-out duration-300 rounded text-base bg-sagegreen hover:bg-quailegg text-white">
-                Post <FontAwesomeIcon icon={faPaperPlane} className="w-4 h-4 pl-2" aria-hidden="true" />
-            </button>
+                setAuthor(e.target.value);
+            }} data-testid="user-input" value={author} type="text" tabIndex={0} id="user-input" disabled={false} className={`text-base rounded-none rounded-l-lg max-w-40 bg-gray-100 dark:bg-gray-100 border text-gray-900 focus:ring-transparent focus:border-transparent block flex-1 min-w-0 transition-width ease-in-out duration-1000 ${handlePostVisibility() ? 'w-0 px-0 py-2.5' : 'w-full p-2.5'} border-gray-200 focus:border-gray-200 dark:bg-gray-200 dark:border-seablue dark:placeholder-gray-400 dark:text-gray-900 dark:focus:ring-none dark:focus:border-none`} placeholder={"Author Name"} />
+            <Tooltip content="Post image to the feed" placement="bottom">
+                <button onClick={(e) => {
+                    handleButtonClick(e)
+                }} disabled={!input.length && !password.length} className={`flex p-2 h-full w-20 ml-1 items-center justify-center transition ease-in-out duration-300 rounded text-base ${handlePostVisibility() ? 'cursor-not-allowed text-gray-600 bg-gray-200 focus:ring-0 hover:ring-transparent' : 'text-white bg-sagegreen/[0.8] hover:bg-sagegreen/[1.0] focus:ring-blue-200 focus:ring-4'}`}>
+                    Post <FontAwesomeIcon icon={faPaperPlane} className="w-4 h-4 pl-2" aria-hidden="true" />
+                </button>
+            </Tooltip>
         </div>
     );
 };
