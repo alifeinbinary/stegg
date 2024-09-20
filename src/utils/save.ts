390b0c811b990bf3c7219edd3e75cef636ef46cb
Andrew Halliwell <studio@strangeways.ca>
2024-09-20 04:52:41 -0700

    feat: adding Toast notifications to Save process

--- src/utils/save.ts
+++ src/utils/save.ts
@@ -18,146 +18,203 @@
 import { addMetadata } from "meta-png";
 import { toast } from "react-toastify";
 
-const toastId = "save";
+let toastId: string;
 
 function saveBlob(blobWithMetadata: Blob, fileName: string) {
-  toast.update(toastId, { render: "Saving image...", type: "success" });
+    toast.update(toastId, {
+        render: "Saving image for download.",
+        type: "success",
+        isLoading: false,
+        autoClose: 2000,
+    });
 
-  const url = URL.createObjectURL(blobWithMetadata);
-  const link = document.createElement("a");
-  link.href = url;
-  link.download = fileName;
+    const url = URL.createObjectURL(blobWithMetadata);
+    const link = document.createElement("a");
+    link.href = url;
+    link.download = fileName;
 
-  document.body.appendChild(link);
-  link.click();
+    document.body.appendChild(link);
+    link.click();
 
-  document.body.removeChild(link);
-  URL.revokeObjectURL(url);
+    document.body.removeChild(link);
+    URL.revokeObjectURL(url);
+}
+
+function returnBlob(blobWithMetadata: Blob, fileName: string) {
+    toast.update(toastId, {
+        render: "Returning blob",
+        type: "info",
+        isLoading: false,
+        autoClose: 2000,
+    });
+
+    const url = URL.createObjectURL(blobWithMetadata);
+    const link = document.createElement("a");
+    link.href = url;
+    link.download = fileName;
+
+    // document.body.appendChild(link);
+    // link.click();
+
+    // document.body.removeChild(link);
+
+    return { fileName, url };
 }
 
 async function addMetadataToPng(
-  blob: Blob,
-  encryptedText: string,
-  encryptionEnabled: string,
-  password: string
+    blob: Blob,
+    encryptedText: string,
+    encryptionEnabled: string,
+    password: string,
 ): Promise<Blob> {
-  toast.update(toastId, { render: "Adding metadata to PNG", type: "info" });
+    toast.update(toastId, { render: "Adding metadata to PNG", type: "info" });
 
-  const buffer = await blob.arrayBuffer();
-  const pngData = new Uint8Array(buffer);
-  const inputText = (document.getElementById("text-input") as HTMLInputElement)
-    .value;
+    const buffer = await blob.arrayBuffer();
+    const pngData = new Uint8Array(buffer);
+    const inputText = (
+        document.getElementById("text-input") as HTMLInputElement
+    ).value;
 
-  if (inputText && password && encryptedText) {
-    const withMessage = addMetadata(pngData, "Message", encryptedText);
-    const withBoolean = addMetadata(
-      withMessage,
-      "Encrypted",
-      encryptionEnabled.toString()
-    );
-    return new Blob([withBoolean], { type: "image/png" });
-  } else {
-    const withMessage = addMetadata(pngData, "Message", inputText);
-    const withBoolean = addMetadata(
-      withMessage,
-      "Encrypted",
-      encryptionEnabled.toString()
-    );
-    return new Blob([withBoolean], { type: "image/png" });
-  }
+    if (inputText && password && encryptedText) {
+        const withMessage = addMetadata(pngData, "Message", encryptedText);
+        const withBoolean = addMetadata(
+            withMessage,
+            "Encrypted",
+            encryptionEnabled.toString(),
+        );
+        return new Blob([withBoolean], { type: "image/png" });
+    } else {
+        const withMessage = addMetadata(pngData, "Message", inputText);
+        const withBoolean = addMetadata(
+            withMessage,
+            "Encrypted",
+            encryptionEnabled.toString(),
+        );
+        return new Blob([withBoolean], { type: "image/png" });
+    }
 }
 
 function getCanvasBlob(canvas: HTMLCanvasElement): Promise<Blob> {
-  return new Promise((resolve) => {
-    canvas.toBlob((blob) => {
-      if (blob) resolve(blob);
-    }, "image/png");
-  });
+    return new Promise((resolve) => {
+        canvas.toBlob((blob) => {
+            if (blob) resolve(blob);
+        }, "image/png");
+    });
 }
 
 async function createPngWithMetadata(
-  canvas: HTMLCanvasElement,
-  encryptedText: string,
-  encryptionEnabled: boolean,
-  password: string
-) {
-  toast.info("Creating PNG from canvas", { toastId: toastId });
+    canvas: HTMLCanvasElement,
+    encryptedText: string,
+    encryptionEnabled: boolean,
+    password: string,
+    action: string,
+): Promise<{ fileName: string; url: string }> {
+    const croppedCanvas = cropImageFromCanvas(canvas);
+    const blob = await getCanvasBlob(croppedCanvas);
 
-  const croppedCanvas = cropImageFromCanvas(canvas);
-  const blob = await getCanvasBlob(croppedCanvas);
+    toast.update(toastId, {
+        render: "Creating PNG with metadata",
+        type: "info",
+    });
+    const blobWithMetadata = await addMetadataToPng(
+        blob,
+        encryptedText,
+        encryptionEnabled.toString(),
+        password,
+    );
 
-  const blobWithMetadata = await addMetadataToPng(
-    blob,
-    encryptedText,
-    encryptionEnabled.toString(),
-    password
-  );
+    if (action === "download") {
+        toast.update(toastId, {
+            render: "Saving image for download",
+            type: "success",
+        });
+        saveBlob(
+            blobWithMetadata,
+            `alifeinbinary_com-${encryptedText.slice(7, 15)}.png`,
+        );
+    } else if (action === "post" && encryptedText) {
+        toast.update(toastId, {
+            render: "Saving image for post",
+            type: "info",
+            autoClose: 2000,
+            isLoading: false,
+        });
+        const filename = `alifeinbinary_com-${encryptedText.slice(7, 15)}.png`;
+        const { url } = returnBlob(blobWithMetadata, filename);
 
-  saveBlob(
-    blobWithMetadata,
-    `alifeinbinary_com-${encryptedText.slice(7, 15)}.png`
-  );
+        return {
+            fileName: filename,
+            url: url,
+        };
+    } else {
+        throw new Error("Invalid action");
+    }
+    return { fileName: "", url: "" };
 }
 
 function cropImageFromCanvas(canvas: HTMLCanvasElement) {
-  toast.info("Trimming canvas", { toastId: toastId });
+    toast.update(toastId, {
+        render: "Trimming the canvas",
+        type: "info",
+        isLoading: true,
+    });
 
-  const ctx = canvas.getContext("2d");
-  if (!ctx) throw new Error("Failed to get canvas context");
+    const ctx = canvas.getContext("2d");
+    if (!ctx) throw new Error("Failed to get canvas context");
 
-  const width = canvas.width;
-  const height = canvas.height;
+    const width = canvas.width;
+    const height = canvas.height;
 
-  const imageData = ctx.getImageData(0, 0, width, height);
-  const data = imageData.data;
+    const imageData = ctx.getImageData(0, 0, width, height);
+    const data = imageData.data;
 
-  let top = height,
-    left = width,
-    right = 0,
-    bottom = 0;
+    let top = height,
+        left = width,
+        right = 0,
+        bottom = 0;
 
-  // Loop through each pixel to find the bounding box of non-transparent pixels
-  for (let y = 0; y < height; y++) {
-    for (let x = 0; x < width; x++) {
-      const alpha = data[(y * width + x) * 4 + 3]; // Get alpha value of the pixel
-      if (alpha > 0) {
-        // If the pixel is not fully transparent
-        if (x < left) left = x;
-        if (x > right) right = x;
-        if (y < top) top = y;
-        if (y > bottom) bottom = y;
-      }
+    // Loop through each pixel to find the bounding box of non-transparent pixels
+    for (let y = 0; y < height; y++) {
+        for (let x = 0; x < width; x++) {
+            const alpha = data[(y * width + x) * 4 + 3]; // Get alpha value of the pixel
+            if (alpha > 0) {
+                // If the pixel is not fully transparent
+                if (x < left) left = x;
+                if (x > right) right = x;
+                if (y < top) top = y;
+                if (y > bottom) bottom = y;
+            }
+        }
     }
-  }
 
-  // Calculate the trimmed width and height
-  const trimmedWidth = right - left + 1;
-  const trimmedHeight = bottom - top + 1;
+    // Calculate the trimmed width and height
+    const trimmedWidth = right - left + 1;
+    const trimmedHeight = bottom - top + 1;
 
-  // Create a new canvas to store the trimmed content
-  const trimmedCanvas = document.createElement("canvas");
-  trimmedCanvas.width = trimmedWidth;
-  trimmedCanvas.height = trimmedHeight + 50;
-  const trimmedCtx = trimmedCanvas.getContext("2d");
-  if (!trimmedCtx) throw new Error("Failed to get trimmed canvas context");
+    // Create a new canvas to store the trimmed content
+    const trimmedCanvas = document.createElement("canvas");
+    trimmedCanvas.width = trimmedWidth;
+    trimmedCanvas.height = trimmedHeight + 50;
+    const trimmedCtx = trimmedCanvas.getContext("2d");
+    if (!trimmedCtx) throw new Error("Failed to get trimmed canvas context");
 
-  // Draw a watermark onto the trimmed canvas
-  trimmedCtx.font = "100 18px Tahoma";
-  trimmedCtx.fontKerning = "none";
-  trimmedCtx.fillStyle = "#ffeedd";
-  trimmedCtx.fillText(
-    "Decode this image at alifeinbinary.com",
-    trimmedWidth - 345,
-    trimmedHeight + 40
-  );
-  // Draw the trimmed content onto the new canvas
-  trimmedCtx.putImageData(
-    ctx.getImageData(left, top, trimmedWidth, trimmedHeight),
-    0,
-    0
-  );
+    // Draw a watermark onto the trimmed canvas
+    trimmedCtx.font = "100 18px Tahoma";
+    trimmedCtx.fontKerning = "none";
+    trimmedCtx.fillStyle = "#ffeedd";
+    trimmedCtx.fillText(
+        "Decode this image at alifeinbinary.com",
+        trimmedWidth - 345,
+        trimmedHeight + 40,
+    );
+    // Draw the trimmed content onto the new canvas
+    trimmedCtx.putImageData(
+        ctx.getImageData(left, top, trimmedWidth, trimmedHeight),
+        0,
+        0,
+    );
 
-  return trimmedCanvas;
+    return trimmedCanvas;
 }
 
-export { saveBlob, createPngWithMetadata, cropImageFromCanvas };
+export { createPngWithMetadata, cropImageFromCanvas, saveBlob };
