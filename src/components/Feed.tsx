74a26ef978d22c5b7be229512a297fa85ed7012b
Andrew Halliwell <studio@strangeways.ca>
2024-09-20 04:47:18 -0700

    feat: dark and light theme, detects user's system theme

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -15,10 +15,10 @@
  *   along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-import { useEffect, useState, FC } from "react";
-import Post from "./Post";
-import { useListBinaryImagePosts } from "../utils/api";
+import { FC, useEffect, useState } from "react";
 import { PostProps } from "../types";
+import { useListBinaryImagePosts } from "../api/listBinaryImagePosts";
+import Post from "./Post";
 
 const Feed: FC = () => {
     const [posts, setPosts] = useState<PostProps[]>([]);
@@ -28,16 +28,27 @@ const Feed: FC = () => {
         if (data && data.listBinaryImagePosts && data.listBinaryImagePosts.data) {
             setPosts(data.listBinaryImagePosts.data);
         }
-
     }, [data]);
 
     if (loading) {
         return (
-            <div className="text-center py-12">
+            <div className="py-12 text-center">
                 <div role="status">
-                    <svg aria-hidden="true" className="inline w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
-                        <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
+                    <svg
+                        aria-hidden="true"
+                        className="mr-2 inline h-8 w-8 animate-spin fill-blue-600 text-gray-200 dark:text-gray-600"
+                        viewBox="0 0 100 101"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path
+                            d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                            fill="currentColor"
+                        />
+                        <path
+                            d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                            fill="currentFill"
+                        />
                     </svg>
                     <span className="sr-only">Loading...</span>
                 </div>
@@ -45,31 +56,50 @@ const Feed: FC = () => {
         );
     }
     if (error) {
-        return <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
-            <strong className="font-bold">Oh dang!</strong>
-            <span className="block sm:inline">There was an error loading the virtual tour.</span>
-            <span className="absolute top-0 bottom-0 right-0 px-4 py-3">
-                <svg className="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" /></svg>
-            </span>
-        </div>;
+        return (
+            <div
+                className="relative rounded border border-red-400 bg-red-100 px-4 py-3 text-red-700"
+                role="alert"
+            >
+                <strong className="font-bold">Oh dang!</strong>
+                <span className="sm:inline block">
+                    There was an error loading the virtual tour.
+                </span>
+                <span className="absolute bottom-0 right-0 top-0 px-4 py-3">
+                    <svg
+                        className="h-6 w-6 fill-current text-red-500"
+                        role="button"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 20 20"
+                    >
+                        <title>Close</title>
+                        <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
+                    </svg>
+                </span>
+            </div>
+        );
     }
     if (!data || !data.listBinaryImagePosts || !data.listBinaryImagePosts.data) {
-        return <p>No data available</p>
+        return <p>No data available</p>;
     }
     return (
-        <div className="feed mt-9">
-            <h4 className="mb-2 h4 sm:hidden xs:hidden text-2xl text-left font-bold dark:text-white">Feed</h4>
-            {posts.map((post) => (
-                <Post
-                    key={post.id}
-                    id={post.id}
-                    author={post.author}
-                    posted={post.posted}
-                    image={post.image}
-                />
-            ))}
+        <div id="feed">
+            <div className="lg:py-8 px-12 max-w-screen-lg mx-auto w-full xs:px-1 md:px-4 pb-1 pt-8">
+                <h4 className="h4 sm:hidden mb-2 text-left text-2xl font-bold dark:text-white xs:hidden">
+                    Feed
+                </h4>
+                {posts.map((post) => (
+                    <Post
+                        key={post.id}
+                        id={post.id}
+                        author={post.author}
+                        posted={post.posted}
+                        image={post.image}
+                    />
+                ))}
+            </div>
         </div>
     );
-}
+};
 
 export default Feed;
--- src/components/Password.tsx
+++ src/components/Password.tsx
@@ -17,6 +17,7 @@
 
 import { useRef } from 'react'
 import { toast } from 'react-toastify';
+import { CustomFlowbiteTheme, Tooltip } from 'flowbite-react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLock, faUnlock } from '@fortawesome/free-solid-svg-icons'
 import { clearContx } from '../utils/translate';
@@ -25,15 +26,17 @@ import { PasswordProps } from '../types';
 
 export const Password: React.FC<PasswordProps> = ({ password, setPassword, setEncryptionEnabled, encryptionEnabled, setOutput, setDecryptedText, decryptedText, setStringToDecrypt, stringToDecrypt, setEncryptedText }) => {
 
-
+    const toastId = useRef("save");
     const canvasRef = useRef<HTMLCanvasElement | null>(null);
 
     const handleCheckboxChange = () => {
         setEncryptionEnabled(!encryptionEnabled)
-        toast.info("Encryption " + (!encryptionEnabled ? "enabled. You will need to provide the secret key to third parties whom you share the image with" : "disabled. Message will be embedded as plain text."), {
-            autoClose: 4000,
-            pauseOnHover: true
-        });
+        toast.update(toastId.current, {
+            render: "Encryption " + (!encryptionEnabled ? "enabled." : "disabled. Message will be embedded as plain text."),
+            type: "info",
+            isLoading: false,
+            autoClose: 1000,
+        })
         if (encryptionEnabled) {
             setPassword('');
         } else {
@@ -52,10 +55,15 @@ export const Password: React.FC<PasswordProps> = ({ password, setPassword, setEn
         }
     }
 
+    const lockTooltop: CustomFlowbiteTheme["tooltip"] = {
+        target: "flex",
+    }
+
     return (
         <label className='relative inline-flex cursor-pointer select-none items-center'>
             <input
                 type='checkbox'
+                name='encryption'
                 checked={encryptionEnabled ? encryptionEnabled : false}
                 onChange={handleCheckboxChange}
                 className='sr-only'
@@ -63,23 +71,21 @@ export const Password: React.FC<PasswordProps> = ({ password, setPassword, setEn
                 data-testid="toggle-encryption"
                 aria-checked={encryptionEnabled}
             />
-            <div id="tooltip-encryption" role="tooltip" className="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
-                Enable AES 128-bit encryption
-                <div className="tooltip-arrow" data-popper-arrow></div>
-            </div>
-            <div tabIndex={0} className='shadow-card p-1 flex h-[46px] items-center justify-center rounded-md text-gray-900 bg-gray-200'>
-                <span
-                    className={`flex h-9 w-9 items-center transition ease-in-out duration-300 justify-center rounded ${!encryptionEnabled ? 'text-sm text-gray-500 bg-gray-200' : 'bg-bergundy hover:bg-deepbergundy text-white'
-                        }`}
-                >
-                    <FontAwesomeIcon icon={faLock} className="w-4 h-4" aria-hidden="true" />
-                </span>
-                <span
-                    className={`flex h-9 w-9 items-center transition ease-in-out duration-300 justify-center rounded ${encryptionEnabled ? 'text-sm text-gray-500 bg-gray-200' : 'bg-lightbergundy hover:bg-bergundy text-white'
-                        }`}
-                >
-                    <FontAwesomeIcon icon={faUnlock} className="w-4 h-4" aria-hidden="true" />
-                </span>
+            <div tabIndex={0} className='shadow-card p-1 flex h-[46px] items-center justify-center rounded-md text-gray-900 bg-gray-200 dark:bg-seablue'>
+                <Tooltip content="Enable AES 128-bit encryption" className='bg-gray-200 dark:bg-beige/[0.8] text-gray-900' trigger="hover" placement="top" theme={lockTooltop}>
+                    <span
+                        className={`flex h-9 w-9 items-center transition ease-in-out duration-300 justify-center rounded-l-md ${!encryptionEnabled ? 'text-sm text-gray-500 bg-gray-200' : 'bg-sagegreen/[0.8] hover:bg-sagegreen/[1.0] text-white'
+                            }`}
+                    >
+                        <FontAwesomeIcon icon={faLock} className="w-4 h-4" aria-hidden="true" />
+                    </span>
+                    <span
+                        className={`flex h-9 w-9 items-center transition ease-in-out duration-300 justify-center rounded-r-md ${encryptionEnabled ? 'text-sm text-gray-500 bg-gray-200' : 'bg-lightbergundy hover:bg-bergundy text-white'
+                            }`}
+                    >
+                        <FontAwesomeIcon icon={faUnlock} className="w-4 h-4" aria-hidden="true" />
+                    </span>
+                </Tooltip>
                 <input onChange={(e) => {
                     setPassword(e.target.value);
                     if (e.target.value.trim() === '') {
@@ -87,7 +93,7 @@ export const Password: React.FC<PasswordProps> = ({ password, setPassword, setEn
                         setDecryptedText('')
                         clearContx(canvasRef);
                     }
-                }} data-testid="password-input" value={password} type="text" tabIndex={0} id="password-input" disabled={!encryptionEnabled} className={`text-base ml-1 rounded-none rounded-e-lg bg-gray-50 border text-gray-900 focus:ring-bergundy focus:border-bergundy block flex-1 min-w-0 max-w-40 w-full border-gray-300 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-bergundy dark:focus:border-bergundy`} placeholder={encryptionEnabled ? 'Enter a secret key' : 'Enable encryption'} />
+                }} data-testid="password-input" value={password} type="text" tabIndex={0} id="password-input" disabled={!encryptionEnabled} className={`text-base ml-1 rounded-none rounded-e-lg bg-gray-100 border text-gray-900 focus:ring-transparent focus:border-gray-200 block flex-1 min-w-0 max-w-40 w-full border-gray-200 p-2.5 dark:bg-gray-200 dark:text-gray-900 dark:border-seablue dark:placeholder-gray-400 dark:focus:ring-transparent dark:focus:border-seablue`} placeholder={encryptionEnabled ? 'Enter a secret key' : 'Enable encryption'} />
             </div>
         </label>
     )
