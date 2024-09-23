4bc0b3bfdd21a4eb5b4a44f35d3888055066b2c3
Andrew Halliwell <studio@strangeways.ca>
2024-09-23 12:25:35 -0700

    refactor: Spinner and Warning to cut down on SVG code for brevity's sake

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -16,9 +16,12 @@
  */
 
 import { FC, useEffect, useState } from "react";
+import { Alert, Spinner } from "flowbite-react";
 import { PostProps } from "../types";
 import { useListBinaryImagePosts } from "../api/listBinaryImagePosts";
 import Post from "./Post";
+import { faInfo } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Feed: FC = () => {
     const [posts, setPosts] = useState<PostProps[]>([]);
@@ -33,50 +36,17 @@ const Feed: FC = () => {
     if (loading) {
         return (
             <div className="py-12 text-center">
-                <div role="status">
-                    <svg
-                        aria-hidden="true"
-                        className="mr-2 inline h-8 w-8 animate-spin fill-blue-600 text-gray-200 dark:text-gray-600"
-                        viewBox="0 0 100 101"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                    >
-                        <path
-                            d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                            fill="currentColor"
-                        />
-                        <path
-                            d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                            fill="currentFill"
-                        />
-                    </svg>
-                    <span className="sr-only">Loading...</span>
-                </div>
+                <Spinner aria-label="Loading" />
+                <span className="sr-only">Loading...</span>
             </div>
         );
     }
     if (error) {
         return (
-            <div
-                className="relative rounded border border-red-400 bg-red-100 px-4 py-3 text-red-700"
-                role="alert"
-            >
-                <strong className="font-bold">Oh dang!</strong>
-                <span className="sm:inline block">
-                    There was an error loading the virtual tour.
-                </span>
-                <span className="absolute bottom-0 right-0 top-0 px-4 py-3">
-                    <svg
-                        className="h-6 w-6 fill-current text-red-500"
-                        role="button"
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 20 20"
-                    >
-                        <title>Close</title>
-                        <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
-                    </svg>
-                </span>
-            </div>
+            <Alert color="failure">
+                <FontAwesomeIcon icon={faInfo} />
+                <span className="font-medium">Oh dang!</span> There was an error loading the posts.
+            </Alert>
         );
     }
     if (!data || !data.listBinaryImagePosts || !data.listBinaryImagePosts.data) {
