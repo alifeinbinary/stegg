e2673a25cf7801421f85e6856b46dc688855aa41
Andrew Halliwell <studio@strangeways.ca>
2024-09-24 03:53:14 -0700

    chore: putting hooks into their own directory

--- src/hooks/useCreateBinaryImagePost.ts
+++ src/hooks/useCreateBinaryImagePost.ts
@@ -16,7 +16,7 @@
  */
 
 import { useMutation } from "@apollo/client";
-import { CREATE_BINARYIMAGEPOST, PUBLISH_BINARYIMAGEPOST } from "./api";
+import { CREATE_BINARYIMAGEPOST, PUBLISH_BINARYIMAGEPOST } from "../api/api";
 
 /*
  * Create a binary image post for the feed
