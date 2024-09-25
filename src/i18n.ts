093dd94792fdc3957bd698405a1c2091feefec16
Andrew Halliwell <studio@strangeways.ca>
2024-09-25 06:07:53 -0700

    fix: making hyperlink text more readable with great colour contrast

--- src/i18n.ts
+++ src/i18n.ts
@@ -21,6 +21,7 @@ import Backend from "i18next-http-backend";
 import { initReactI18next } from "react-i18next";
 
 i18n
+
     // load translation using http -> see /public/locales
     // learn more: https://github.com/i18next/i18next-http-backend
     .use(Backend)
@@ -31,10 +32,11 @@ i18n
     .use(initReactI18next)
     // init i18next
     // for all options read: https://www.i18next.com/overview/configuration-options
+
     .init({
         lng: "en",
         fallbackLng: "en",
-        debug: true,
+        debug: import.meta.env.MODE === "development",
         backend: {
             loadPath: "./public/locales/{{lng}}/{{ns}}.json",
         },
