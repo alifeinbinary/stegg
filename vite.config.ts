2636a44ff292c9606867222496487d86dd82d591
Andrew Halliwell <studio@strangeways.ca>
2024-09-25 15:44:05 -0700

    fix: removing /binary-translate/ as vite.config base since files weren't resolving on gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: "/binary-translate/",
+    base: "/",
     build: {
         assetsDir: "public",
     },
