98a83d54afeb151093aec601ad8ceb39c4c996a0
Andrew Halliwell <studio@strangeways.ca>
2024-09-26 17:59:20 -0700

    feat: build time sitemap generation, purgecss and tree shaking

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,28 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import Sitemap from "vite-plugin-sitemap";
+import { purgeCss } from "vite-plugin-tailwind-purgecss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        Sitemap({
+            hostname: "https://feed.alifeinbinary.com",
+            i18n: {
+                defaultLanguage: "en",
+                languages: ["en", "es", "fr"],
+                strategy: "prefix",
+            },
+        }),
+        purgeCss(),
+    ],
     base: "/",
     build: {
         assetsDir: "public",
+        sourcemap: true,
+        rollupOptions: {
+            treeshake: "recommended",
+        },
     },
 });
