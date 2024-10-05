7ce659250c7faeeb99d5f35c06ee56cb4310f437
Andrew Halliwell <studio@strangeways.ca>
2024-10-04 20:02:56 -0700

    fix: set an env var so reverting all base url and homepage vars back to the state they were in before this mess started

--- vite.config.ts
+++ vite.config.ts
@@ -4,28 +4,26 @@ import Sitemap from "vite-plugin-sitemap";
 import { purgeCss } from "vite-plugin-tailwind-purgecss";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-    return {
-        plugins: [
-            react(),
-            Sitemap({
-                hostname: "https://stegg.alifeinbinary.com/",
-                i18n: {
-                    defaultLanguage: "en",
-                    languages: ["en", "es", "fr"],
-                    strategy: "prefix",
-                },
-            }),
-            purgeCss(),
-        ],
-        base: mode === "production" ? "/" : "/",
-        build: {
-            assetsDir: "public",
-            sourcemap: true,
-            rollupOptions: {
-                treeshake: "recommended",
+export default defineConfig({
+    plugins: [
+        react(),
+        Sitemap({
+            hostname: "https://stegg.alifeinbinary.com/",
+            i18n: {
+                defaultLanguage: "en",
+                languages: ["en", "es", "fr"],
+                strategy: "prefix",
             },
-            minify: "esbuild",
+        }),
+        purgeCss(),
+    ],
+    base: "/",
+    build: {
+        assetsDir: "public",
+        sourcemap: true,
+        rollupOptions: {
+            treeshake: "recommended",
         },
-    };
+        minify: "esbuild",
+    },
 });
