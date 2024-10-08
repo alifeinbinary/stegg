46aec5c916cf854c5b596e4f05afc156ac082ac3
Andrew Halliwell <studio@strangeways.ca>
2024-10-08 15:07:58 -0700

    console logging env vars to confirm existence, using gh-pages deploy instead of actions.

--- src/main.tsx
+++ src/main.tsx
@@ -11,15 +11,16 @@ import router from './routes.tsx';
 const readApiHttpLink = createHttpLink({
   uri: import.meta.env.VITE_READ_API_URL,
 });
-console.log("readApiHttpLink", readApiHttpLink)
+console.log("readApiHttpLink", import.meta.env.VITE_READ_API_URL)
 const mainApiHttpLink = createHttpLink({
   uri: import.meta.env.VITE_MAIN_API_URL,
 });
-console.log("mainApiHttpLink", mainApiHttpLink)
+console.log("mainApiHttpLink", import.meta.env.VITE_MAIN_API_URL)
 const manageApiHttpLink = createHttpLink({
   uri: import.meta.env.VITE_MANAGE_API_URL,
 });
-console.log("manageApiHttpLink", manageApiHttpLink)
+console.log("manageApiHttpLink", import.meta.env.VITE_MANAGE_API_URL)
+
 const cache = new InMemoryCache({
   typePolicies: {
     Query: {
