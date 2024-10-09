d5b1e27c97bfbcd56ddb08c989edd89bccab4154
Andrew Halliwell <studio@strangeways.ca>
2024-10-09 02:49:25 -0700

    Updates

---
+++ public/useTheme-lcV3gjwQ.js
@@ -0,0 +1,2 @@
+import{r as t}from"./index-BeumOYdf.js";const m=()=>{const[e,a]=t.useState(localStorage.theme==="dark"||!("theme"in localStorage)&&window.matchMedia("(prefers-color-scheme: dark)").matches?"dark":"light");return t.useEffect(()=>{document.documentElement.classList.toggle("dark",e==="dark")},[e]),{theme:e,toggleTheme:()=>{a(e==="dark"?"light":"dark"),localStorage.theme=e==="dark"?"light":"dark"}}};export{m as u};
+//# sourceMappingURL=useTheme-lcV3gjwQ.js.map
